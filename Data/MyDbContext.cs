5fe0c6b86737146aaf0b6301cda7872fb3a25be0
Minh Nhat <minhnhat012340@gmail.com>
2024-02-22 16:32:12 +0700

    initial

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using GroupProject_Ecommerce.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GroupProject_Ecommerce.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
+using GroupProject_Ecommerce.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
