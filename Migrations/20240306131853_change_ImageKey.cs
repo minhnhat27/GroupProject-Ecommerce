a10fa1d3bfcaa83e5b5d82e6d86404f8dcb46192
Minh Nhat <minhnhat012340@gmail.com>
2024-03-06 20:21:13 +0700

    add Migration change_ImageKey

---
+++ Migrations/20240306131853_change_ImageKey.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroupProject_Ecommerce.Migrations
+{
+    /// <inheritdoc />
+    public partial class change_ImageKey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Images",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<double>(
+                name: "Price",
+                table: "Products",
+                type: "float",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Images",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Images",
+                table: "Images",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_ProductId",
+                table: "Images",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Images",
+                table: "Images");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Images_ProductId",
+                table: "Images");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Images");
+
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Products",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(double),
+                oldType: "float");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Images",
+                table: "Images",
+                column: "ProductId");
+        }
+    }
+}
