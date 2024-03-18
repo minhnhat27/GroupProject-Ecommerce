e78ffb5bbbd4d71f37e64ea0619c0cfea1651599
AnthonyPA0902 <ioephuocan@gmail.com>
2024-03-18 18:36:03 +0700

    Xong giao dien trang chu, trang san pham, loc san pham theo loai

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
+using GroupProject_Ecommerce.Data;
 using GroupProject_Ecommerce.Models;
-using Microsoft.AspNetCore.Identity;
+using GroupProject_Ecommerce.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace GroupProject_Ecommerce.Controllers
@@ -8,15 +10,28 @@ namespace GroupProject_Ecommerce.Controllers
     //[Authorize]
     public class HomeController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
+		private readonly MyDbContext _context;
+		public HomeController(MyDbContext context)
+		{
+			_context = context;
+		}
+		public IActionResult Index()
+		{
+			var viewModel = new HomeViewModel
+			{
+				Categories = _context.Categories.ToList(),
+				ImagesWithProducts = _context.Images
+					.Include(img => img.Product)
+					.ToList()
+			};
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			return View(viewModel);
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GroupProject_Ecommerce.Models;
+
+namespace GroupProject_Ecommerce.ViewModels
+{
+	public class HomeViewModel
+	{
+		public List<Category> Categories { get; set; }
+		public List<Image> ImagesWithProducts { get; set; }
+	}
+}
